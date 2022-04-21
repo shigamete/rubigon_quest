b6226cde98b83331405045789a46f516cfdb64da
shigamete <daiki_02@ymail.ne.jp>
2022-04-22 08:12:14 +0900

    デプロイのため調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "rubigons#index"
+  # get "rubigons/show_1"
   resources :rubigons do
     resources :acts, only: [:create, :edit, :update, :destroy]
   end
