3fcf9331de675a086c5fc43f65a10a5045c60c0a
shigamete <daiki_02@ymail.ne.jp>
2022-04-22 08:14:16 +0900

    ビューファイル削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "rubigons#index"
-  # get "rubigons/show_1"
   resources :rubigons do
     resources :acts, only: [:create, :edit, :update, :destroy]
   end
